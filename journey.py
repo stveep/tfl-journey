3c6f858cb48703422681ebe7ccb2fb5528c12dfa
stveep <spettitt@gmail.com>
2017-08-07 08:26:22 +0100

    Add a list of stations from https://www.doogal.co.uk/london_stations.php

--- journey.py
+++ journey.py
@@ -1,7 +1,7 @@
 import requests 
-import urlparse
+from numpy import arange
+import csv
 
-start_points = ["SW1A1AA", "E202ST", "51.505896,-0.166802", "W1A1AA"]
 end_point = "SW36JB"
 date = "20170904"
 time = "0800"
@@ -9,7 +9,11 @@ time = "0800"
 def plan_journey(start_point, end_point, date, time):
   url = "https://api.tfl.gov.uk/Journey/JourneyResults/" + start_point + "/to/" + end_point
   payload = {'date': date, 'time': time}
-  return requests.get(url, payload).json()
+  r = requests.get(url, payload)
+  if r.status_code == requests.codes.ok:
+    return r.json()
+  else:
+    return False # Fails silently if the request fails
 
 # Response structure:
 # {"journeys": [
@@ -18,7 +22,21 @@ def plan_journey(start_point, end_point, date, time):
 def durations(response):
   return map(lambda x: x['duration'], response['journeys'])
 
-for i in start_points:
-  r = plan_journey(i, end_point, date, time)
-  print i + "\t" + str(min(durations(r)))
+
+locations = []
+header = True
+with open('stations.csv', 'rb') as csvfile:
+  reader = csv.reader(csvfile)
+  for row in reader: 
+    if header:
+      header = False
+      next
+    # Filter by Zone:
+    elif row[5] in ['1','2','3']:
+      locations.append([row[0],row[6]])
+
+for point in locations:
+  r = plan_journey(point[1], end_point, date, time)
+  if r:
+    print point[0] + "\t" + str(min(durations(r)))
 
